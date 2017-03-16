b2715a3b724abf0f6cfdf2fb5b60378f6c26425b
woodmerce <alexandre.mercier.425@hotmail.com>
2017-03-16 16:44:43 -0400

    Revert "fuckoff"
    This reverts commit fcbb4743ad3a6b6149954ccb6c4e4444197f85db.
    
    # Conflicts:
    #	Xtreme.vb

--- Xtreme.vb
+++ Xtreme.vb
@@ -14,12 +14,17 @@ Public Class Xtreme
         NomtableTout = {"Achats", "Adresses des employés", "Clients", "Commandes", "Détails des commandes", "Employés", "Fournisseurs", "Info Xtreme", "Produits", "Régions", "Types de produit"}
         NomTable = {"Clients", "Fournisseurs", "Produits", "Employés", "Types de produit"}
         listeTXT_Four = {txt_four_1, txt_four_2, Txt_four_3, Txt_four_4, Txt_four_5, Txt_four_7, txt_four_6, Txt_four_8}
-
+        listeTXT_Produit = {Txt_prod_1, Txt_prod_2, Txt_prod_3, Txt_prod_4, Txt_prod_5, Txt_prod_6, Txt_prod_7, Txt_prod_8}
         listeTXT_Employes = {Txt_Emp_1, Txt_Emp_2, Txt_Emp_3, Txt_Emp_4, Txt_Emp_5, Txt_Emp_6, Txt_Emp_7, txt_Emp_10, Txt_Emp_8, Txt_Emp_9, Txt_Emp_11, Txt_Emp_12, Txt_Emp_16, Txt_Emp_13, Txt_Emp_14, Txt_Emp_15}
         listeTXT_Client = {txt_Clients_1, txt_Clients_2, txt_Clients_3, txt_Clients_4, txt_Clients_5, txt_Clients_6, txt_Clients_7, txt_Clients_8, txt_Clients_9, txt_Clients_10, txt_Clients_11, txt_Clients_12, txt_Clients_13, txt_Clients_14, txt_Clients_15}
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         listeTXT = {listeTXT_Client, listeTXT_Four, listeTXT_Employes}
+=======
+        listeTXT_Type_Produit = {Txt_Type_prod_1, Txt_Type_prod_2, Txt_Type_prod_3, Txt_Type_prod_4}
+        listeTXT = {listeTXT_Client, listeTXT_Four, listeTXT_Produit, listeTXT_Employes}
+>>>>>>> parent of fcbb474... fuckoff
         listPanel = {pan_clients, pan_Fournisseur, Type_prod_4, Pan_employer}
 =======
         listeTXT = {listeTXT_Client, listeTXT_Four, listeTXT_Produit, listeTXT_Employes}
